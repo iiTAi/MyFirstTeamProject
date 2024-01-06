710e3b94cb6bec62a8cc5efd35776d5b3c6978eb
tsuki917 <einstrom917@gmail.com>
2024-01-07 01:35:48 +0900

    add redo undo

--- src/drawjs/fabric.js
+++ src/drawjs/fabric.js
@@ -1,7 +1,53 @@
 const canvas = new fabric.Canvas("canvas");
 canvas.backgroundColor = "white";
+let stackSwitch = false;
+const Undo_register = []
+const Redo_register = []
 
-document.getElementById("draw").addEventListener("click", function () {
+
+canvas.on('object:added', () =>{
+  if(stackSwitch)return;
+  console.log("object:added")
+  Undo_register.push(JSON.stringify(canvas));
+  console.log(Undo_register)
+  Redo_register.length = 0
+});
+
+const undo = () => {
+  console.log("undo")
+  if (Undo_register.length > 0) {
+    stackSwitch = true;
+    if (Undo_register.length > 1) {
+      Redo_register.push(Undo_register.pop())
+      const content = Undo_register[Undo_register.length - 1];
+      
+      canvas.loadFromJSON(content, ()=> {
+        canvas.renderAll();
+        stackSwitch = false;
+      });
+
+    }else if(Undo_register.length==1){
+      Redo_register.push(Undo_register.pop())
+      canvas.clear();
+    } 
+  }
+  console.log(Undo_register.length)
+}
+const redo = ()=>{
+  if (Redo_register.length > 0) {
+    stackSwitch = true;
+    const content = Redo_register.pop();
+    Undo_register.push(content);
+    canvas.loadFromJSON(content, ()=> {
+      canvas.renderAll();
+      stackSwitch=false;
+    });
+  }
+}
+document.getElementById("undo").addEventListener("click",undo)
+document.getElementById("redo").addEventListener("click",redo)
+
+document.getElementById("draw").addEventListener("click", ()=> {
   canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
   canvas.freeDrawingBrush.width = document.getElementById("weight").value;
   canvas.freeDrawingBrush.color = "black";

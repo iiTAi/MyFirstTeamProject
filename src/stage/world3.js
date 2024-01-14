456b416a0e81850840c13113f36d77b20ec994f6
iiTAi <kimagureowata@icloud.com>
2024-01-15 01:38:50 +0900

    ステージ3の実装

--- src/stage/world3.js
+++ src/stage/world3.js
@@ -443,21 +443,256 @@ function world3() {
                 // インスタンスの更新
                 chr = new CharaClass();
                 bg = new BackGroundClass();
-                obj = [];
+                obj = [
+                    new ClearLine(1, 2),
+
+                    new ColorChanger(2, 2, "inv"),
+                    new ColorChanger(2, 2, "inv"),
+                    new ColorChanger(1, 2, "inv"),
+                    new ColorChanger(2, 1, "inv"),
+                    new ColorChanger(2, 3, "inv"),
+                    new ColorChanger(2, 1, "inv"),
+                    new ColorChanger(2, 2, "inv"),
+
+                    new ColorChanger(1, 2, "red"),
+                    new ColorChanger(2, 1, "red"),
+                    new ColorChanger(1, 1, "red"),
+                    new ColorChanger(1, 1, "red"),
+                    new ColorChanger(2, 1, "red"),
+                    new ColorChanger(2, 1, "red"),
+                    new ColorChanger(2, 1, "red"),
+
+                    new ColorChanger(1, 1, "green"),
+                    new ColorChanger(2, 5, "green"),
+                    new ColorChanger(1, 1, "green"),
+                    new ColorChanger(1, 1, "green"),
+                    new ColorChanger(2, 1, "green"),
+                    new ColorChanger(2, 1, "green"),
+
+                    new WarpPortal("floor", 0, 1),
+                    new WarpPortal("floor", 1, 0),
+                    
+                    new OneWayWall(1, 2, "right"),
+                    new OneWayWall(1, 2, "left"),
+
+                    new TransRectClass(17, 1),
+                    new TransRectClass(6, 1),
+                    new TransRectClass(7, 1),
+                    new TransRectClass(8, 1),
+                    new TransRectClass(6, 1),
+                    new TransRectClass(4, 1),
+                    new TransRectClass(1, 2),
+                    new TransRectClass(2, 2),
+                    new TransRectClass(1, 2),
+
+                    new RectClass(4, 2),
+                    new RectClass(4, 1),
+                    new RectClass(1, 7),
+                    new RectClass(12, 1),
+                    new RectClass(3, 4),
+                    new RectClass(5, 1),
+                    new RectClass(12, 1),
+                    new RectClass(1, 9),
+                    new RectClass(1, 10),
+                    new RectClass(17, 2),
+                    new RectClass(1, 19),
+                    new RectClass(2, 5 ),
+                    new RectClass(2, 6),
+                    new RectClass(1, 16),
+                    new RectClass(1, 1),
+                    new RectClass(1, 1),
+                    new RectClass(1, 1),
+
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+                    new GravityButton("floor"),
+
+                    new GravityButton("ceiling"),
+                    new GravityButton("ceiling"),
+                    new GravityButton("ceiling"),
+                    new GravityButton("ceiling"),
+                    new GravityButton("ceiling"),
+
+                    new RectClass(1, 1),
+                    new GravityButton("floor"),
+                    new GravityButton("floor")
+                ];
+
 
                 // 初期化
+                bg.setRGB(0, 0, 0);
+                chr.init(0, 19, 255, 255, 255);
+                obj[0].init(37, 18, 255, 255, 255);
+
+                obj[1].init(5, 18, 0, 0, 0);
+                obj[2].init(9, 18, 0, 0, 0);
+                obj[3].init(13, 18, 0, 0, 0);
+                obj[4].init(17, 15, 0, 0, 0);
+                obj[5].init(5, 11 , 0, 0, 0);
+                obj[6].init(29, 13, 0, 0, 0);
+                obj[7].init(31, 9, 0, 0, 0);
+
+                obj[8].init(16, 16, 255, 0, 0);
+                obj[9].init(19, 15, 255, 0, 0);
+                obj[10].init(1, 6, 255, 0, 0);
+                obj[11].init(27, 6, 255, 0, 0);
+                obj[12].init(29, 5, 255, 0, 0);
+                obj[13].init(33, 6, 255, 0, 0);
+                obj[14].init(33, 14, 255, 0, 0);
+
+                obj[15].init(2, 10, 0, 255, 0);
+                obj[16].init(5, 5, 0, 255, 0);
+                obj[17].init(20, 1, 0, 255, 0);
+                obj[18].init(22, 4, 0, 255, 0);
+                obj[19].init(29, 14, 0, 255, 0);
+                obj[20].init(33, 5, 0, 255, 0);
+
+                obj[21].init(2, 19, 255, 255, 255);
+                obj[22].init(10, 6, 255, 255, 255);
+
+                obj[23].init(7, 8, 255, 255, 255);
+                obj[24].init(14, 5, 255, 255, 255);
+
+                obj[25].init(4, 20, 255, 255, 255);
+                obj[26].init(4, 14, 255, 0, 0);
+                obj[27].init(10, 14 , 255, 255, 255);
+                obj[28].init(0, 0, 0, 255, 0);
+                obj[29].init(15, 7, 255, 255, 255);
+                obj[30].init(17, 10, 255, 255, 255);
+                obj[31].init(21, 8, 255, 255, 0);
+                obj[32].init(36, 9, 255, 255, 0);
+                obj[33].init(28, 18, 0, 0, 0);
+
+                obj[34].init(0, 20, 255, 255, 255);
+                obj[35].init(0, 15, 255, 255, 255);
+                obj[36].init(4, 4, 255, 255, 255);
+                obj[37].init(5, 10, 255, 255, 255);
+                obj[38].init(7, 4, 255, 255, 255);
+                obj[39].init(10, 7, 255, 255, 255);
+                obj[40].init(10, 4, 255, 255, 255);
+                obj[41].init(21, -1, 255, 255, 255);
+                obj[42].init(21, 10, 255, 255, 255);
+                obj[43].init(21, 20, 255, 255, 255);
+                obj[44].init(28, -1, 255, 255, 255);
+                obj[45].init(31, 4, 255, 255, 255);
+                obj[46].init(31, 11, 255, 255, 255);
+                obj[47].init(35, 4, 255, 255, 255);
+                obj[48].init(30, 0, 255, 255, 255);
+                obj[49].init(33, 0, 255, 255, 255);
+                obj[50].init(36, 0, 255, 255, 255);
+                obj[66].init(24, -1, 255, 255, 255);
+
+                obj[51].init(0, 14, 255, 255, 255);
+                obj[52].init(11, 3, 255, 255, 255);
+                obj[53].init(14, 3, 255, 255, 255);
+                obj[54].init(15, 3, 255, 255, 255);
+                obj[55].init(19, 3, 255, 255, 255);
+                obj[56].init(22, 10, 255, 255, 255);
+                obj[57].init(23, 10, 255, 255, 255);
+                obj[58].init(25, 15, 255, 255, 255);
+                obj[59].init(26, 15, 255, 255, 255);
+                obj[60].init(27, 15, 255, 255, 255);
+                obj[67].init(20, 19, 255, 255, 255);             
+                obj[68].init(31, 19, 255, 255, 255);             
+
+                obj[61].init(7, 1, 255, 255, 255);
+                obj[62].init(24, 0, 255, 255, 255);
+                obj[63].init(12, 5, 255, 255, 255);
+                obj[64].init(36, 1, 255, 255, 255);
+                obj[65].init(11, 11, 255, 255, 255);
+
+                // スタート地点の設定
+                setStartPoint(chr);
 
                 // stateの更新
                 state = "draw";
 
             } else if (state == "draw") {
                 // 背景の描画
+                bg.push();
 
                 // 各種処理
+                obj[1].checkClash(chr);
+                obj[2].checkClash(chr);
+                obj[3].checkClash(chr);
+                obj[4].checkClash(chr);
+                obj[5].checkClash(chr);
+                obj[6].checkClash(chr);
+                obj[7].checkClash(chr);
+                obj[8].checkClash(chr);
+                obj[9].checkClash(chr);
+                obj[10].checkClash(chr);
+                obj[11].checkClash(chr);
+                obj[12].checkClash(chr);
+                obj[13].checkClash(chr);
+                obj[14].checkClash(chr);
+                obj[15].checkClash(chr);
+                obj[16].checkClash(chr);
+                obj[17].checkClash(chr);
+                obj[18].checkClash(chr);
+                obj[19].checkClash(chr);
+                obj[20].checkClash(chr);
+
+                obj[21].checkClash(chr, obj);
+                obj[22].checkClash(chr, obj);
+
+                obj[23].setClash(chr);
+                obj[24].setClash(chr);
+
+                obj[25].setClash(bg);
+                obj[26].setClash(bg);
+                obj[27].setClash(bg);
+                obj[28].setClash(bg);
+                obj[29].setClash(bg);
+                obj[30].setClash(bg);
+                obj[31].setClash(bg);
+                obj[32].setClash(bg);
+                obj[33].setClash(bg);
+   
+                obj[51].checkClash(chr);
+                obj[52].checkClash(chr);
+                obj[53].checkClash(chr);
+                obj[54].checkClash(chr);
+                obj[55].checkClash(chr);
+                obj[56].checkClash(chr);
+                obj[57].checkClash(chr);
+                obj[58].checkClash(chr);
+                obj[59].checkClash(chr);
+                obj[60].checkClash(chr);
+                obj[61].checkClash(chr);
+                obj[62].checkClash(chr);
+                obj[63].checkClash(chr);
+                obj[64].checkClash(chr);
+                obj[65].checkClash(chr);
+                obj[67].checkClash(chr);
+                obj[68].checkClash(chr);
+
+                clash(chr, obj);
+                chr.checkOffScreen();
+                chr.move();
 
                 // 描画
-
+                chr.push();
+                pushes(obj);
+                // images load from firestrage
+                if (!isImgLoad) {
+                  getStageImg(stage);
+                  isImgLoad = true;
+                }
                 // ゴール判定
+                if (obj[0].checkClear(chr)) {
+                  nextstage = "4-1";
+                  state = "clear";
+                  isImgLoad = false;
+                  deleteStageImg();
+                }
             }
             break;
         }

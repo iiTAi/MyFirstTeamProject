fe07485f0a107c56074e36d40057d28c7eaeaa35
ZuyaTepo <mimunesato@gmail.com>
2024-01-16 22:07:32 +0900

    ワールド４のステージ１を作成

--- src/main/main.js
+++ src/main/main.js
@@ -2,7 +2,7 @@
 
 /* 変数の定義 */
 let scene = "title"; // 場面を制御する変数
-let stage = "4-1"; // 実行するステージを制御する変数
+let stage = "1-1"; // 実行するステージを制御する変数
 let nextstage = null; // 次のステージを一時的に保持する変数
 let state = "setup"; // 各ステージでの初期化とループを制御する変数
 let gravity = "down"; // 重力の向きを制御する変数
@@ -142,7 +142,7 @@ function draw() {
 /* キーが入力された瞬間のみ呼び出される関数 */
 function keyPressed() {
   if (scene == "title") {
-    scene = "world2";
+    scene = "world1";
   }
   switch (key) {
     case "w": {

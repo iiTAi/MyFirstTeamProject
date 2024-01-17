4ad8b74ef88b23fd2591266ad26d727c84ecc97c
iiTAi <kimagureowata@icloud.com>
2024-01-17 13:11:33 +0900

    ゲームクリア時のテキスト修正

--- src/main/main.js
+++ src/main/main.js
@@ -33,6 +33,7 @@ var obj_text = [
   new TextClass("RESTART:R", 50, false),
   new TextClass("WORLD: " + world, 50, false),
   new TextClass("STAGE: " + stage, 50, false),
+  new TextClass("THANK YOU FOR PLAYING!", 100, false),
 ];
 
 /* 事前読み込み */
@@ -58,6 +59,8 @@ function setup() {
   obj_text[7].init(2, 4, 255, 255, 255);
   obj_text[8].init(2, 5, 255, 255, 255);
   obj_text[9].init(2, 6, 255, 255, 255);
+  obj_text[12].init(6, 13, 255, 255, 255);
+  
 }
 
 /* ループ */
@@ -148,6 +151,7 @@ function draw() {
           obj_text[4].push();
         } else {
           obj_text[3].push();
+          obj_text[12].push();
         }
         flag[0] = true;
       }
